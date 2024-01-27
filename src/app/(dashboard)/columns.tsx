82d04fba3537f9211eae527b3ea0a0c0e1914b3e
alimarief <alimarief97@gmail.com>
2024-01-27 20:31:15 +0700

    Added datatable

---
+++ src/app/(dashboard)/columns.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type Product = {
+  id: string;
+  name: String;
+  description: String;
+  price: number;
+  rating: number;
+  image: String;
+  qty: number;
+};
+
+export const columns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "image",
+    header: "Image",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "rating",
+    header: "Rating",
+  },
+  {
+    accessorKey: "qty",
+    header: "Stock",
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem className="text-red-500">Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import React, { FC } from 'react'
+import React, { FC } from "react";
 import {
   Table,
   TableBody,
@@ -8,48 +8,53 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import { Product } from "@prisma/client";
 
-interface HomeProps {
-  
-}
+interface HomeProps {}
 
-const Home: FC<HomeProps> = ({  }) => {
+const Home: FC<HomeProps> = ({}) => {
    const data = [
-    { id: 1, name: 'Product 1', price: 200000, rating: 4, qty:20 },
-    { id: 2, name: 'Product 2', price: 300000,  rating: 5, qty:2},
+    { id: 'a',name: 'ha', description: 'had',image: 'ah', qty:20, price: 20000, rating: 5, createdAt: null },
+    { id: 'b',name: 'ha1', description: 'had1',image: 'ah1', qty:30, price: 130000, rating: 5, createdAt: null },
+    { id: 'c',name: 'ha2', description: 'had2',image: 'ah2', qty:40, price: 350000, rating: 5, createdAt: null },
     // Add more data as needed
   ];
 
   return (
-    <div>
-      <h1>Products</h1>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>No</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Rating</TableHead>
-            <TableHead>Qty</TableHead>
-            
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>{item.id}</TableCell>
-              <TableCell>{item.name}</TableCell>
-              <TableCell>{item.price}</TableCell>
-              <TableCell>{item.rating}</TableCell>
-              <TableCell>{item.qty}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        
-      </Table>
+    // <div>
+    //   <h1>Products</h1>
+    //   <Table>
+    //     <TableHeader>
+    //       <TableRow>
+    //         <TableHead>No</TableHead>
+    //         <TableHead>Name</TableHead>
+    //         <TableHead>Price</TableHead>
+    //         <TableHead>Rating</TableHead>
+    //         <TableHead>Qty</TableHead>
+
+    //       </TableRow>
+    //     </TableHeader>
+    //     <TableBody>
+    //       {data.map((item) => (
+    //         <TableRow key={item.id}>
+    //           <TableCell>{item.id}</TableCell>
+    //           <TableCell>{item.name}</TableCell>
+    //           <TableCell>{item.price}</TableCell>
+    //           <TableCell>{item.rating}</TableCell>
+    //           <TableCell>{item.qty}</TableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+
+    //   </Table>
+    // </div>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
-  )
-}
+  );
+};
 
 export default Home;

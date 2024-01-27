b54b67c226960154b06cea54cbb6839c1b3085f7
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2024-01-27 19:35:46 +0700

    table

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,53 @@
+"use client"
+
 import React, { FC } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 interface HomeProps {
   
 }
 
 const Home: FC<HomeProps> = ({  }) => {
+   const data = [
+    { id: 1, name: 'Product 1', price: 200000, rating: 4, qty:20 },
+    { id: 2, name: 'Product 2', price: 300000,  rating: 5, qty:2},
+    // Add more data as needed
+  ];
+
   return (
     <div>
-     Products
+      <h1>Products</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>No</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Rating</TableHead>
+            <TableHead>Qty</TableHead>
+            
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.price}</TableCell>
+              <TableCell>{item.rating}</TableCell>
+              <TableCell>{item.qty}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        
+      </Table>
     </div>
   )
 }
--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,13 +1,45 @@
 import React, { FC } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 interface UsersPageProps {
-  
+
 }
 
-const UsersPage: FC<UsersPageProps> = ({  }) => {
+const UsersPage: FC<UsersPageProps> = ({ }) => {
+
+  const data = [
+    { id: 1, name: "nikky", email: "nikki@mail.com" },
+    { id: 2, name: "alim", email: "alim@mail.com" }
+  ]
   return (
     <div>
-     UsersPage
+      <h1>Customer</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>No</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.email}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
